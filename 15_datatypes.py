6573eefe5e95586eefe1e308992a79961d193d0e
RAVI-DAHIYA--00 <ravidahiya9350@gmail.com>
2024-08-20 18:29:29 +0530

    learning

--- 15_datatypes.py
+++ 15_datatypes.py
@@ -1,3 +1,6 @@
+#what is data type????
+#data type specifies the type of value 
+
 #In Python, data types define the kind of values a variable can hold. Here are the built-in data types:
 
 #types of data type in python
@@ -6,3 +9,80 @@
 #3. boolean data type
 #4. sequenced data type
 #5. mapped data type
+
+
+
+
+
+
+
+
+#what is numeric data type????
+#numeric data type specifies the numeric value like int ,float,complex.
+
+
+#int:represensts the integer value.
+#float:represents the floating value.
+#complex:represents the complex value.
+
+
+
+
+#example of numeric data type
+a = 1  #integer data type
+b = 2.5  #float data type
+c = complex(2, 3)  #complex data type
+
+
+print('\n', a)     #'\n' is used for new line
+print(b)
+print(c)
+
+
+
+
+
+
+
+
+
+#what is text data type????
+#text data type specifies the text value like str,string.
+
+#str:represents the string value.
+#string:represents the string value.
+
+#example of text data type
+a = 'hello'  #string data type
+b = 'world'  #string data type
+
+
+print(a, '\n', b)
+
+print(a[3])    #thus will give the number 3 character in a.
+
+
+
+
+
+
+
+
+
+#what is boolean data type????
+#boolean data type specifies the next value like true false.
+
+#example of boolean data type
+a = True  #boolean data type     #T should be capital
+b = False  #boolean data type     #F should be capital
+
+print(a)
+print(b)
+print(a, b)
+
+#use of boolean data type in the real world.
+a=1
+b=2
+var_a=a<b
+print(var_a)
+print(type[var_a])
--- learning.py
+++ learning.py
@@ -152,47 +152,17 @@ from typing import TYPE_CHECKING, SupportsComplex                           #thi
 
 
 
-#what is numeric data type????
-#numeric data type specifies the numeric value like int ,float,complex.
 
-#int:represensts the integer value.
-#float:represents the floating value.
-#complex:represents the complex value.
 
-#what is data type????
-#data type specifies the type of value 
 
-#example of numeric data type
-a = 1  #integer data type
-b = 2.5  #float data type
-c = complex(2, 3)  #complex data type
 
-print('\n', a)     #'\n' is used for new line
-print(b)
-print(c)
 
-#what is text data type????
-#text data type specifies the text value like str,string.
 
-#str:represents the string value.
-#string:represents the string value.
 
-#example of text data type
-a = 'hello'  #string data type
-b = 'world'  #string data type
 
-print(a, '\n', b)
 
-#what is boolean data type????
-#boolean data type specifies the next value like true false.
 
-#example of boolean data type
-a = True  #boolean data type     #T should be capital
-b = False  #boolean data type     #F should be capital
 
-print(a)
-print(b)
-print(a, b)
 
 #what is sequenced data type?    ???
 #sequenced data type specifies the next value like list,tuple.
