692ba2451dcfec3cb60c2fa5e1f51e63ebe2bf88
RAVI-DAHIYA--00 <ravidahiya9350@gmail.com>
2024-10-20 23:04:32 +0530

    File changes

--- z_keynotes.py
+++ z_keynotes.py
@@ -1,7 +1,16 @@
-# Example two-digit number
-number = 78
+# # Example two-digit number
+# number = 78
 
-# Find the first digit
-first_digit = number // 10
+# # Find the first digit
+# first_digit = number // 10
 
-print("The first digit is:", first_digit)
+# print("The first digit is:", first_digit)
+
+
+
+
+w=input()
+s=set(w)
+v=("a","e","i","o","u")
+d=s.intersection(v)
+print(d)
